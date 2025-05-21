1a02ac1c35828bf415d8289467bcf44c7f706c91
negod <silvajoaopedrossn@gmail.com>
2025-05-21 20:33:06 -0300

    ajuste

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -3,10 +3,19 @@ import pacienteRoutes from "./routes/pacientes.js"
 import cors from "cors"
 
 const app = express()
+const port = 3001;
 
 app.use(express.json())
 app.use(cors())
 
 app.use("/", pacienteRoutes)
+
+app.get('/', (req, res) => {
+  res.send('Backend funcionando!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
 
 app.listen(5000)
